20b905ff7ed9ddd05383dcc7b64379d41fa64bd5
Aftab Alam Khan <aftab.khan@pager.com>
2022-05-22 18:49:49 -0400

    ecom web

---
+++ src/GetImage.jsx
@@ -0,0 +1,39 @@
+import one from './images/1.webp';
+import two from './images/2.webp';
+import three from './images/3.jpeg';
+import c1 from './images/c1.jpeg';
+import c2 from './images/c2.jpeg';
+import c3 from './images/c3.jpeg';
+import p1 from './images/p1.png';
+import p2 from './images/p2.webp';
+import p3 from './images/p3.png';
+import p4 from './images/p4.png';
+import p5 from './images/p5.png';
+import p6 from './images/p6.png';
+import p7 from './images/p7.webp';
+import p8 from './images/p8.png';
+import pay from './images/pay.png'
+
+const images = {
+  one,
+  two,
+  three,
+  c1,
+  c2,
+  c3,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  pay,
+};
+
+function GetImage(key) {
+  return images[key]
+}
+
+export default GetImage
---
+++ src/data.js
@@ -0,0 +1,77 @@
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'one',
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVVALS.",
+    bg: "f5fafd"
+  },
+  {
+    id: 2,
+    img: 'two',
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVVALS.",
+    bg: "fcf1ed"
+  },
+  {
+    id: 3,
+    img: 'three',
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVVALS.",
+    bg: "fbf0f4"
+  }
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "c1",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "c2",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "c3",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"p1",
+  },
+  {
+    id:2,
+    img:"p2",
+  },
+  {
+    id:3,
+    img:"p3",
+  },
+  {
+    id:4,
+    img:"p4",
+  },
+  {
+    id:5,
+    img:"p5",
+  },
+  {
+    id:6,
+    img:"p6",
+  },
+  {
+    id:7,
+    img:"p7",
+  },
+  {
+    id:8,
+    img:"p8",
+  },
+]
